2830fa8000bbc0b8c4ed6b7ec1c7cac0c5ed2e0f
Sergey Dokin <dokin.serg@gmail.com>
2024-09-27 20:10:25 +0300

    2024-09-27

---
+++ Duble_Track.sql
@@ -0,0 +1,8 @@
+﻿Select tracknumber, artist, locale, title , Count(*) as cct
+from PlaylistUpdatable 
+Where playlist_name = 'Dokin' 
+group by tracknumber 
+having (cct != 1)
+order by tracknumber
+;
+
